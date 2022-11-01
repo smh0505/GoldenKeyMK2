cbfe4cfa01f424d69822d0d7e3dd408186e4ea1d
Minho Son <42821865+smh0505@users.noreply.github.com>
2022-11-01 09:40:02 -0400

    Initial Commit

---
+++ Wheel.cs
@@ -0,0 +1,69 @@
+using System.Numerics;
+using Raylib_cs;
+
+namespace GoldenKeyMK2
+{
+    public struct Option
+    {
+        public string Name;
+        public Color Color;
+        public int Count;
+
+        public Option(string name, Color color, int count)
+        {
+            this.Name = name;
+            this.Color = color;
+            this.Count = count;
+        }
+    }
+
+    public class Wheel
+    {
+        public static List<Option> Options = new List<Option>();
+        public static List<string> WaitingOptions = new List<string>();
+        private static Random rnd = new Random();
+
+        public static int Sum
+        {
+            get
+            {
+                int sum = 0;
+                foreach (var option in Options) sum += option.Count;
+                return sum;
+            }
+        }
+
+        public static void NewOption(string name)
+        {
+            var color = new Color(rnd.Next(256), rnd.Next(256), rnd.Next(256), 255);
+            var newOption = new Option(name, color, 1);
+            Options.Add(newOption);
+        }
+
+        public static void AddOption(string name)
+        {
+            int id = -1;
+            foreach (var option in Options) if (option.Name == name) id = Options.IndexOf(option);
+            if (Options.Count == 0) NewOption(name);
+            else if (id == -1) NewOption(name);
+            else
+            {
+                var newOption = new Option(Options[id].Name, Options[id].Color, Options[id].Count + 1);
+                Options.RemoveAt(id);
+                Options.Insert(id, newOption);
+            }
+        }
+
+        public static void DrawWheel(float startAngle)
+        {
+            var center = new Vector2(450, 360);
+            var currAngle = startAngle;
+            foreach (var option in Options)
+            {
+                Raylib.DrawCircleSector(center, 300, currAngle, currAngle + 360f / Sum * option.Count, 0, Raylib.Fade(option.Color, 0.3f));
+                Raylib.DrawCircleSectorLines(center, 300, currAngle, currAngle + 360f / Sum * option.Count, 0, option.Color);
+                currAngle += 360f / Sum * option.Count;
+            }
+        }
+    }
+}
